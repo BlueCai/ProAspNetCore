d9e73edcc3fdf55d674685a93881ef651f8c30fe
qibang <wuyicqb@163.com>
2016-10-13 23:17:57 +0800

    init

---
+++ SportsStore/src/SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public int PageSize = 4;
+
+        public ProductController(IProductRepository repo)
+        {
+            this.repository = repo;
+        }
+        // GET: /<controller>/
+        public IActionResult Index(int page = 1)
+        {
+            return View(repository.Products.OrderBy(p=>p.ProductID).Skip((page - 1) * PageSize).Take(PageSize));
+        }
+    }
+}
