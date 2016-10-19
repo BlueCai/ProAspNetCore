d2f96dc457e70488e585cdf685d5f6e3f19b62f0
qibang <wuyicqb@163.com>
2016-10-19 23:25:09 +0800

    init

---
+++ ControllersAndActions/src/ControllersAndActions/Controllers/PocoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ControllersAndActions
+{
+    public class PocoController
+    {
+        //public string Index() => "This is a POCO Controller";
+
+        public ViewResult Index() => new ViewResult()
+        {
+            ViewName = "Result",
+            ViewData = new ViewDataDictionary(
+                new EmptyModelMetadataProvider(),
+                new ModelStateDictionary())
+                {
+                    Model = $"This is a Poco Controller"
+                }
+
+        };
+    }
+}
